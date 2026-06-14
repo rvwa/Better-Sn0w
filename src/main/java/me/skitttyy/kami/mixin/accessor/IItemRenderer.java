df1e17f847bcba36720d3a0bac6c0c75163fedbc
05yj on discord <svegr.124@gmail.com>
2026-06-14 11:28:19 +0000

    Remove unused accessor from IItemRenderer
    Removed accessor for BuiltinModelItemRenderer from IItemRenderer interface.

--- src/main/java/me/skitttyy/kami/mixin/accessor/IItemRenderer.java
+++ src/main/java/me/skitttyy/kami/mixin/accessor/IItemRenderer.java
@@ -1,21 +1,16 @@
 package me.skitttyy.kami.mixin.accessor;
 
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.item.BuiltinModelItemRenderer;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ItemRenderer.class)
 public interface IItemRenderer
 {
-    @Accessor("builtinModelItemRenderer")
-    BuiltinModelItemRenderer getBuiltInModelItemRenderer();
-
     @Invoker("renderBakedItemModel")
     void renderBakedItemModelAccess(BakedModel model, ItemStack stack, int light, int overlay, MatrixStack matrices, VertexConsumer vertices);
 }
